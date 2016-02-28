8be2994cc8c45c7c1b2f4043488a3d4ca444f946
Andrew Servania <andymasterservania@hotmail.com>
2016-02-28 15:56:28 +0100

    + Removed Legend label

--- AudioPlayer/Form1.Designer.cs
+++ AudioPlayer/Form1.Designer.cs
@@ -53,10 +53,12 @@
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.IsValueShownAsLabel = true;
+            series1.IsVisibleInLegend = false;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(931, 507);
+            this.chart1.Size = new System.Drawing.Size(931, 457);
             this.chart1.TabIndex = 0;
             // 
             // button1
